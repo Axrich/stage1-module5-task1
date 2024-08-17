1da6286938441dab698464e5a0583ec1f45811dc
Axrich <116589250+Axrich@users.noreply.github.com>
2024-08-17 18:17:04 +0500

    Update InterfaceCreator.java

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,7 +1,6 @@
 package com.epam.mjc;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -11,22 +10,76 @@ import java.util.function.Supplier;
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return sourceList -> {
+            boolean actualResult = true;
+            for (String value : sourceList) {
+                char[] charValue = value.toCharArray();
+                int valueFirstLetter = charValue[0];
+                if (!Character.isUpperCase(valueFirstLetter)) {
+                    actualResult = false;
+                    break;
+                }
+
+            }
+            return actualResult;
+        };
     }
 
+
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        return sourceList -> {
+            List<Integer> resultList = new ArrayList<>();
+            resultList.clear();
+
+                for (int value : sourceList) {
+                if (value % 2 == 0) {
+                    resultList.add(value);
+                }
+
+            }
+            sourceList.addAll(resultList);
+        };
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> result = new ArrayList<>();
+        {
+            return () -> {
+                for (String value : values) {
+                    if (Character.isUpperCase(value.charAt(0)) && value.endsWith(".") && value.length() > 3) {
+                        result.add(value);
+                    }
+
+                }
+                return result;
+
+            };
+        }
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        return sourceKeys -> {
+            Map<String, Integer> resultMap = new HashMap<>();
+            for (String key : sourceKeys) {
+                Integer value = key.length();
+                resultMap.put(key, value);
+            }
+            return resultMap;
+        };
     }
 
+
+
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+
+        return (sourceList1, sourceList2) -> {
+            List<Integer> resultList = new ArrayList<>();
+            resultList.addAll(sourceList1);
+            resultList.addAll(sourceList2);
+            return resultList;
+        };
     }
 }
